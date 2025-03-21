e92109ebd857af3c043f7515f334aa453b789343
Thanasud <thanasud.r@kkumail.com>
2025-03-21 21:10:23 +0700

    add

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Options.tscn")
+	get_tree().change_scene_to_file("res://scenes/option_menu.tscn")
 
 
 func _on_quit_button_pressed() -> void:
